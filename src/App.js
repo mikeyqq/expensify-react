eb75d6c0947fb7dabe3d160ee588e0fd5cd202e5
Anchen <acmhuang@gmail.com>
2019-10-07 19:48:43 -0400

    removed unnecessary folders/added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const ExpenseDashboardPage = () => <div>This is from my dashboard component</div>;
+
+const AddExpensePage = () => <div>This is from my add expense component</div>;
+
+const EditExpensePage = () => <div>This is from my edit expense component</div>;
+
+const HelpPage = () => <div>This is from my help component</div>;
+
+const NotFoundPage = () => <div>404!</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
