a525e359e17f614a4afef2510aa4dd398bf64804
Anchen <acmhuang@gmail.com>
2019-10-08 20:45:34 -0400

    clean up router

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const EditExpensePage = () => <div>This is from my edit expense component</div>;
+const EditExpensePage = props => {
+  console.log(props);
+  return <div>This is from my edit expense component</div>;
+};
 
 export default EditExpensePage;
