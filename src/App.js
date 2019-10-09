b3f9b5bac668a382ebfbf0a3e26c33be4cdbd545
Anchen <acmhuang@gmail.com>
2019-10-08 20:16:43 -0400

    organized routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import AppRouter from "../src/routers/AppRouter";
 import "normalize.css/normalize.css";
 import "./styles/components/styles.scss";
 
-const ExpenseDashboardPage = () => <div>This is from my dashboard component</div>;
-
-const AddExpensePage = () => <div>This is from my add expense component</div>;
-
-const EditExpensePage = () => <div>This is from my edit expense component</div>;
-
-const HelpPage = () => <div>This is from my help component</div>;
-
-const NotFoundPage = () => <div>404!</div>;
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <Link to="/">Dashboard</Link>
-    <Link to="/create">Create Expense</Link>
-    <Link to="/edit">Edit Expense</Link>
-    <Link to="/help">Help</Link>
-  </header>
-);
-
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" component={ExpenseDashboardPage} exact={true} />
-          <Route path="/create" component={AddExpensePage} />
-          <Route path="/edit" component={EditExpensePage} />
-          <Route path="/help" component={HelpPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div>
+      <AppRouter />
+    </div>
   );
 }
 
