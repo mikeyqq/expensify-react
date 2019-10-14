080729bf1d0eef2d8ec640525033e315a2ee99b2
Anchen <acmhuang@gmail.com>
2019-10-14 13:56:07 -0400

    added dropdown menu for sorting feature

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setTextFilter } from "../actions/filters";
-
+import { setTextFilter, sortByDate, sortByAmount } from "../actions/filters";
 //WE GET DISPATCH AND STATE OF FILTERS THROUGH USING CONNECT
 const ExpenseListFilters = props => (
   <div>
@@ -10,9 +9,17 @@ const ExpenseListFilters = props => (
       value={props.filters.text}
       onChange={e => {
         props.dispatch(setTextFilter(e.target.value));
-        console.log(e.target.value);
       }}
     />
+    <select
+      value={props.filters.sortBy}
+      onChange={e => {
+        e.target.value === "date" ? props.dispatch(sortByDate()) : props.dispatch(sortByAmount());
+      }}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 );
 
