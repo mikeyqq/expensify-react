3a0882677726447c23c0bcc57b86b7b6f53dd061
Anchen <acmhuang@gmail.com>
2019-10-14 15:59:19 -0400

    created/expenseform-comp, used regular expression for conditioning

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,10 @@
 import React from "react";
-
-const AddExpensePage = () => <div>This is from my add expense component</div>;
+import ExpenseForm from "./ExpenseForm";
+const AddExpensePage = () => (
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm />
+  </div>
+);
 
 export default AddExpensePage;
