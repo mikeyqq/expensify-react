4adf424cf0c4b3b37e6fb5fdef44e6a337a26499
Anchen <acmhuang@gmail.com>
2019-10-15 14:19:52 -0400

    setup jest testing for actions/reducers/selectors

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,47 @@
+import moment from "moment";
+import { setStartdate, setEndDate, setTextFilter, sortByAmount, sortByDate } from "../../actions/filters";
+
+test("should generate set start date", () => {
+  const action = setStartdate(moment(0));
+  expect(action).toEqual({
+    type: "SET_START_DATE",
+    startDate: moment(0)
+  });
+});
+
+test("should generate set end date", () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: "SET_END_DATE",
+    endDate: moment(0)
+  });
+});
+
+test("should generate set text filter object with text value", () => {
+  const text = "Something in";
+  const action = setTextFilter(text);
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text
+  });
+});
+
+test("should generate set text filter object with default", () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
+  });
+});
+
+test("should generate action object sort by date", () => {
+  expect(sortByDate()).toEqual({
+    type: "SORT_BY_DATE"
+  });
+});
+
+test("should generate action object sort by amount", () => {
+  expect(sortByAmount()).toEqual({
+    type: "SORT_BY_AMOUNT"
+  });
+});
