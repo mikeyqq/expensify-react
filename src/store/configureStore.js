adec070189bbacbb6297fd080733718eaeb2e39d
Anchen <acmhuang@gmail.com>
2019-10-16 13:23:49 -0400

    fixed issue in configurestore where expenseReducer had typo error causing typeError

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,7 +7,7 @@ import filtersReducer from "../reducers/filters";
 export default () => {
   const store = createStore(
     combineReducers({
-      expense: expenseReducer,
+      expenses: expenseReducer,
       filters: filtersReducer
     }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
