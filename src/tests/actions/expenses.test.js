0c0abcbab334c49e38dc1a3d888cc96626ca6284
Anchen <acmhuang@gmail.com>
2019-10-17 11:11:53 -0400

    fixed jest testing in action generator for expenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -9,22 +9,22 @@ test("should setup remove expense action object", () => {
 });
 
 test("should setup edit expense action object", () => {
-  const action = editExpense("123abc", { note: "test" });
+  const action = editExpense("123abc", { note: "new value" });
   expect(action).toEqual({
     type: "EDIT_EXPENSE",
     id: "123abc",
     updates: {
-      note: "test"
+      note: "new value"
     }
   });
 });
 
 test("should setup add expense action object with provided values", () => {
   const expenseData = {
-    description: "Rent",
-    amount: 109500,
+    description: "test1",
+    amount: 12200,
     createdAt: 1000,
-    note: "rent-test"
+    note: "test1"
   };
   const action = addExpense(expenseData);
   expect(action).toEqual({
@@ -36,16 +36,16 @@ test("should setup add expense action object with provided values", () => {
   });
 });
 
-test("should setup add expense action object with default values", () => {
+test("should setup add expense with default", () => {
   const action = addExpense();
   expect(action).toEqual({
     type: "ADD_EXPENSE",
     expense: {
-      id: expect.any(String),
       description: "",
       note: "",
       amount: 0,
-      createdAt: 0
+      createdAt: 0,
+      id: expect.any(String)
     }
   });
 });
--- src/tests/components/Header.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
-import Header from "../../components/Header";
-
-test("Should render Header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-});
